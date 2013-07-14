99bdc6cee516c3a799ff7ad048f53f18f3dad849
Liam Burke <liam@wormholegames.com>
2013-07-14 00:51:40 -0700

    Output now in fancy paragraph format.
    Instead of that ugly list. Also, recognizes min_repeat, subpatterns and
    references to them, handles sets of literals better, and fixed a bug
    while handling unexpected start_groupings.

--- speakregex.py
+++ speakregex.py
@@ -1,20 +1,20 @@
 import io
 import re
 import sys
+import textwrap
+import itertools
 from politer import polite
 
 # Functions for getting and formatting the parse tree for a regular expression.
 
 def line_and_indent(line):
-    '''Takes a line beginning with whitespace, returns the stripped line and
-    the length of the whitespace.
+    '''Dedents a line, returns it with the amount of previous indentation.
     '''
     stripped_line = line.lstrip()
     return stripped_line, len(line) - len(stripped_line)
 
 def parse_regex(regex_string, debug=True):
-    '''Takes a regular expression string and returns the parse tree generated
-    by the compiler when compiling it, as a string.
+    '''Returns the parse tree for a regular expression, as a string.
     
     Rather than reinvent the wheel, get_parse_tree just compiles the regex you
     pass it with the debug flag set and temporarily redirects stdout to catch
@@ -32,7 +32,8 @@ def parse_regex(regex_string, debug=True):
 
 @polite
 def get_parse_tree(regex_string):
-    '''Takes a regular expression and generates the elements in the parse tree.
+    '''Yields the parse tree for a regular expression, element by element.
+    
     Indicates the beginning and end of a group of indented lines by generating
     fake 'start_grouping' and 'end_grouping' elements.
     '''
@@ -51,35 +52,43 @@ def get_parse_tree(regex_string):
     
 # Translation functions.
     
-def repeating(lexemes, tree, delegate):
-    if next(tree) != 'start_grouping 0':
-        raise ValueError
+def repeat(lexemes, tree, delegate):
+    start_grouping(tree)
     min, max = lexemes[1], lexemes[2]
-    leadin = "between {0} and {1}:\n  ".format(min, max)
+    greed = " (non-greedy)" if lexemes[0] == "min_repeat" else ""
+    leadin = "between {0} and {1}{2} occurrences of ".format(min, max, greed)
     subset = translate(tree)
     return leadin + "\n  ".join(subset)
     
-def collect_literals(lexemes, tree, delegate):
+def literal(lexemes, tree, delegate):
     literals = [lexemes[1]]
     for item in tree:
-        next_command, next_character = item.split()
+        more_lexemes = item.split()
+        next_command, next_character = more_lexemes[0], more_lexemes[1]
         if next_command != 'literal':
             tree.send(item)
             break
         literals.append(next_character)
     characters = [chr(int(literal)) for literal in literals]
+    if delegate == 'set':
+        last_character = characters.pop()
+        character_list = "', '".join(characters)
+        return "a '{0}' or '{1}' character".format(character_list,
+                                                   last_character)
     if len(characters) == 1:
         return "the character '{0}'".format(characters[0])
     return "the characters '{0}'".format("".join(characters))
 
-def explicit_set(lexemes, tree, delegate):
-    if next(tree) != 'start_grouping 0':
-        raise ValueError
-    item_descs = translate(tree, delegate='set')
-    return "One of the following: " + ",".join(item_descs)
+def regex_in(lexemes, tree, delegate):
+    start_grouping(tree)
+    item_descs = list(translate(tree, delegate='set'))
+    if len(item_descs) == 1:
+        return item_descs[0]
+    else:
+        return "one of the following: " + ", ".join(item_descs)
 
 categories = {
-    'category_word': "an alphanumeric character or underscore",
+    'category_word': "any alphanumeric character or underscore",
     'category_space': "any whitespace character",
     'category_not_word': "any non-alphanumeric character",
 }
@@ -89,9 +98,28 @@ def category(lexemes, tree, delegate):
     no_such_category = "an unknown category: {0}".format(cat_type)
     return categories.get(cat_type, no_such_category)
 
-# Some fake 'translation' functions to help iterate over the flattened tree.
+def subpattern(lexemes, tree, delegate):
+    start_grouping(tree)
+    pattern_name = lexemes[1]
+    subpattern = speechify(translate(tree))
+    if pattern_name == 'None':
+        subpattern_template = "a non-captured subpattern ({0})"
+    else:
+        subpattern_template = "subpattern {1} ({0})"
+    return subpattern_template.format(subpattern, pattern_name)
     
-def clean_up_stack(lexemes, tree, delegate):
+def groupref(lexemes, tree, delegate):
+   pattern_name = lexemes[1]
+   return "subpattern {0} again".format(pattern_name)
+    
+# Some fake 'translation' functions to help iterate over the flattened tree.
+
+def start_grouping(tree):
+    if next(tree) != 'start_grouping 0':
+        raise ValueError
+    return True
+    
+def unexpected_start_grouping(lexemes, tree, delegate):
     '''Handle 'start_grouping' by removing the following 'end_grouping'.
     
     'start_grouping' is a fake element produced by get_parse_tree when we reach
@@ -101,7 +129,7 @@ def clean_up_stack(lexemes, tree, delegate):
     iterate into the tree to find and remove the 'end_grouping' element we know
     is there, then restore the other elements.
     '''
-    elements = [element for element in tree]
+    elements = list(itertools.takewhile(lambda x: x != 'end_grouping 0', tree))
     for element in reversed(elements):
         tree.send(element)
     return "(warning, some elements may not appear correctly grouped)"
@@ -119,12 +147,15 @@ def end_grouping(lexemes, tree, delegate):
 # and translation functions.
     
 translation = {
-    'max_repeat': repeating,
-    'literal': collect_literals,
-    'in': explicit_set,
+    'max_repeat': repeat,
+    'min_repeat': repeat,
+    'literal': literal,
+    'in': regex_in,
     'category': category,
     'end_grouping': end_grouping,
-    'start_grouping': clean_up_stack,
+    'start_grouping': unexpected_start_grouping,
+    'subpattern': subpattern,
+    'groupref': groupref,
 }
 
 def translate(tree, delegate=None):
@@ -139,11 +170,12 @@ def translate(tree, delegate=None):
     
 # The actual function!
 
+def speechify(translation):
+    return ", followed by ".join(translation)
+
 def speak(regex_string):
     tree = get_parse_tree(regex_string)
-    translation = ["This regular expression will match: "]
-    translation.extend(translate(tree))
-    make_speech(translation)
-    
-def make_speech(chunks):
-    print("\n".join(chunks))
+    translation = translate(tree)
+    speech_template = "This regular expression will match {0}."
+    speech = speech_template.format(speechify(translation))
+    print(textwrap.fill(speech))
