88673338b5f990c497af9b2f60d55e6709ce87a2
Liam Liwanag Burke <liam@wormholegames.com>
2013-07-21 21:10:12 -0700

    OxfordList, Measurable classes, collapsible_list, conjoined.
    Classes to help handle generators like lists without actually making
    them lists. Functions to prettify and collapse sublists.

--- speakregex.py
+++ speakregex.py
@@ -2,6 +2,7 @@ import io
 import re
 import sys
 import textwrap
+import collections
 import itertools
 from politer import polite, politer
 
@@ -23,6 +24,49 @@ special_characters = {
     '92': 'a backslash',
 }
 
+unusual_characters = {
+    '59': 'a semicolon',
+    '61': 'an equals sign',
+}
+
+## Classes
+
+class OxfordList(collections.Iterable):
+    '''A generator wrapper that separates out the last item.
+    
+    When handling text it is often useful, for grammatical reasons, to separate
+    the last item from a list -- but with generators this is harder to do. To
+    make this easier, an OxfordList delivers all the items in a list but one
+    when iterated over, then places that item in its 'last_item' attribute.
+    '''
+    def __init__(self, iterable):
+        self.generator = iter(iterable)
+    def __iter__(self):
+        item = next(self.generator)
+        while True:
+            try:
+                next_item = next(self.generator)
+            except StopIteration:
+                self.last_item = item
+                raise
+            yield item
+            item = next_item 
+
+
+class Measurable(collections.Iterator):
+    def __init__(self, iterable):
+        self.generator = iter(iterable)
+    def __next__(self):
+        return next(self.generator)
+    def at_least(self, length):
+        self.generator, measure = itertools.tee(self.generator)
+        try:
+            for i in range(length):
+                next(measure)
+        except StopIteration:
+            return False
+        return True
+
 ## Functions
 
 # Text-handling functions.
@@ -37,8 +81,6 @@ def line_and_indent(line):
 def lookup_char(text_ordinal):
     '''Takes an ordinal, returns a readable representation of that character.
     '''
-    if text_ordinal in special_characters:
-        return special_characters[text_ordinal]
     return chr(int(text_ordinal))
 
 
@@ -54,19 +96,43 @@ def inline_list(items, internal_sep="", ending_sep=""):
     if internal_sep:
         internal_sep += " "
     separator = ", {0}".format(internal_sep)
-    item_list = list(items)
-    if not ending_sep:
-        return separator.join(item_list)
-    last_item = item_list.pop()
-    return "{0} {1} {2}".format(separator.join(item_list), ending_sep,
-                                last_item)
+    if ending_sep:
+        items = Measurable(conjoined(items, ending_sep))
+    if items.at_least(3):
+        return separator.join(items)
+    return " ".join(items)
+
+
+def conjoined(items, conjunction):
+    measure_items = Measurable(items)
+    if not measure_items.at_least(3):
+        first_item, last_item = next(measure_items), next(measure_items)
+        yield "{0} {1} {2}".format(first_item, conjunction, last_item)
+        raise StopIteration
+    item_list = OxfordList(measure_items)
+    yield from item_list
+    last_item = conjunction + " " + item_list.last_item
+    yield last_item
 
 
 def is_bulleted(line):
     return line.lstrip().startswith("*")
 
 
-def bullet_list(lines):
+def collapsible_list(lines, intro_line="", paren=False):
+    ending = ")" if paren else ""
+    lines = Measurable(lines)
+    if not lines.at_least(2):
+        yield intro_line + " " + next(lines) + ending
+        raise StopIteration
+    lines = OxfordList(bullet_list(lines, intro_line))
+    yield from lines
+    yield lines.last_item + ending
+    
+
+def bullet_list(lines, intro_line=None):
+    if intro_line:
+        yield intro_line + ":"
     for line in lines:
         leader = "  " if is_bulleted(line) else " * "
         line = leader + line
@@ -83,17 +149,12 @@ def clean_up_syntax(lines):
 
 
 def punctuate(lines):
-    lines = politer(lines)
+    lines = OxfordList(lines)
     for line in lines:
-        try:
-            check_if_last = next(lines)
-            lines.send(check_if_last)
-        except StopIteration:
-            break
         if not line.endswith(":"):
             line = line + ","
         yield line
-    line = line + "."
+    line = lines.last_item + "."
     yield line
 
 
@@ -104,7 +165,6 @@ def wrapped_list(lines):
         wrapper.initial_indent = " " * indent
         wrapper.subsequent_indent = " " * (indent + 5)
         yield from wrapper.wrap(stripped)
-    
 
 # Functions for getting and formatting the parse tree.
 
@@ -153,12 +213,17 @@ def repeat(lexemes, tree, delegate):
     min, max = lexemes[1], lexemes[2]
     greed = " (non-greedy)" if lexemes[0] == "min_repeat" else ""
     if int(max) >= max_match:
-        leadin = "{0} or more{1} occurrences of ".format(min, greed)
+        leadin = "{0} or more{1} occurrences of".format(min, greed)
+    elif int(min) == 0:
+        if int(max) == 1:
+            leadin = "up to {0}{1} occurrence of".format(max, greed)
+        else:
+            leadin = "up to {0}{1} occurrences of".format(max, greed)
     else:
-        leadin = "between {0} and {1}{2} occurrences of ".format(min, max,
+        leadin = "between {0} and {1}{2} occurrences of".format(min, max,
                                                                  greed)
-    subset = translate(tree)
-    return leadin + inline_list(subset, "followed by")
+    subset = clean_up_syntax(translate(tree))
+    return collapsible_list(subset, leadin)
 
 
 def collect_literals(tree):
@@ -174,9 +239,11 @@ def collect_literals(tree):
 def literal(lexemes, tree, delegate):
     character = lexemes[1]
     if character in special_characters:
-        return lookup_char(character)
+        return special_characters[character]
     literals = [character,] + list(collect_literals(tree))
     if len(literals) == 1:
+        if character in unusual_characters:
+            return unusual_characters[character]
         return "the character {0}".format(*quoted_chars(literals))
     if delegate == 'set':
         char_list = inline_list(quoted_chars(literals), ending_sep="or")
@@ -187,17 +254,19 @@ def literal(lexemes, tree, delegate):
 
 def regex_in(lexemes, tree, delegate):
     start_grouping(tree)
-    item_descs = list(translate(tree, delegate='set'))
+    set_items = translate(tree, delegate='set')
+    item_descs = list(set_items)
     if len(item_descs) == 1:
         return item_descs[0]
     else:
-        return "one of the following: " + inline_list(item_descs,
-                                                    ending_sep="or")
+        conjoined_descs = conjoined(item_descs, "or")
+        return bullet_list(conjoined_descs, "one of the following:")
 
 categories = {
     'category_word': "any alphanumeric character or underscore",
     'category_space': "any whitespace character",
     'category_not_word': "any non-alphanumeric character",
+    'category_digit': "any digit",
 }
     
 def category(lexemes, tree, delegate):
@@ -209,17 +278,17 @@ def category(lexemes, tree, delegate):
 def subpattern(lexemes, tree, delegate):
     start_grouping(tree)
     pattern_name = lexemes[1]
-    subpattern = inline_list(translate(tree), "followed by")
     if pattern_name == 'None':
-        subpattern_template = "a non-captured subpattern ({0})"
+        subpattern_intro = "a non-captured subgroup consisting of"
     else:
-        subpattern_template = "subpattern {1} ({0})"
-    return subpattern_template.format(subpattern, pattern_name)
-    
+        subpattern_intro = "subgroup #{0}, consisting of".format(pattern_name)
+    subpattern = translate(tree)
+    return collapsible_list(subpattern, subpattern_intro)
+
 
 def groupref(lexemes, tree, delegate):
    pattern_name = lexemes[1]
-   return "subpattern {0} again".format(pattern_name)
+   return "subgroup #{0} again".format(pattern_name)
 
     
 def regex_any(lexemes, tree, delegate):
@@ -228,10 +297,10 @@ def regex_any(lexemes, tree, delegate):
     
 def assert_not(lexemes, tree, delegate):
     start_grouping(tree)
-    negated_pattern = inline_list(translate(tree), "followed by")
+    negated_pattern = list(clean_up_syntax(translate(tree)))
     attached_element = next(translate(tree))
-    negation = "{0} (unless preceded by {1})"
-    return negation.format(attached_element, negated_pattern)
+    negation = "{0} (unless preceded by".format(attached_element)
+    return collapsible_list(negated_pattern, negation, paren=True)
 
 
 locations = {
@@ -248,9 +317,9 @@ def regex_at(lexemes, tree, delegate):
 def regex_range(lexemes, tree, delegate):
     range_regex = r'[0-9]+'
     range_values = (re.findall(range_regex, lexeme) for lexeme in lexemes[1:])
-    range_list = (quoted_chars(val[0] for val in range_values))
+    range_limits = quoted_chars(val[0] for val in range_values)
     range_desc = "a character between {0} and {1}"
-    return range_desc.format(*range_list)
+    return range_desc.format(*range_limits)
     
 # Some fake 'translation' functions to help iterate over the flattened tree.
 
@@ -278,7 +347,11 @@ def unexpected_start_grouping(lexemes, tree, delegate):
     iterate into the tree to find and remove the 'end_grouping' element we know
     is there, then restore the other elements.
     '''
-    elements = list(itertools.takewhile(lambda x: x != 'end_grouping 0', tree))
+    elements = []
+    for element in tree:
+        if element == 'end_grouping 0':
+            break
+        elements.append(element)
     for element in reversed(elements):
         tree.send(element)
     return "(warning, some elements may not appear correctly grouped)"
@@ -332,11 +405,11 @@ def translate(tree, delegate=None):
 def speak(regex_string=None):
     if regex_string is None:
         regex_string = input("Enter a regular expression (unquoted, please):")
+    speech_intro = "This regular expression will match"
     tree = get_parse_tree(regex_string)
     translation = translate(tree)
     syntax_pass = clean_up_syntax(translation)
     punctuation_pass = punctuate(syntax_pass)
-    bulleted_pass = bullet_list(punctuation_pass)
-    final_translation = wrapped_list(bulleted_pass)
-    print("This regular expression will match:")
+    bulleted_translation = collapsible_list(punctuation_pass, speech_intro)
+    final_translation = wrapped_list(bulleted_translation)
     print("\n".join(final_translation))
